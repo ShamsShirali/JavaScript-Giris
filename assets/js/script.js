c7c3fb25846674f3a025f2893b1677273eedb4af
unknown <shamsshsh@code.edu.az>
2023-03-28 00:06:40 +0400

    javascript

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+let number=prompt('Reqemi daxil edin:');
+
+if(number>0){
+    console.log("Eded musbetdir)");
+}
+else if(number<0){
+    console.log("Eded menfidir!")
+}
+else{
+    console.log("Eded sifirdir;)")
+}
